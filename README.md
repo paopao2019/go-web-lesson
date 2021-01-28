f286498e6f72c0025480a68e36a4fde1622e98ec
ANJI-ALLWAYS\cuizhiliang <735896273@qq.com>
2021-01-28 22:30:20 +0800

    学习登入验证码

--- README.md
+++ README.md
@@ -28,3 +28,25 @@
 	学习如何使用gin 返回json数据
 	- 方法1. 使用 gin.H 自己拼接json 其实就是map
 	- 方法2. 使用结构体 还要学会灵活使用tag对结构体字段做定制化操作
+
+
+## lesson26 中间件middleware JWT 
+	- 学习如何使用JWT
+	- CreateToken
+	- ParseToken
+	- 加BufferTime 原理
+
+## lesson27 学习casbin
+	数据来自mysql  https://github.com/casbin/gorm-adapter
+	学习资料来自: https://casbin.org/docs/zh-CN/overview
+	- 演示casbin基本使用
+	- Adapter基于mysql, 策略存入mysql
+	- 如何增加策略 删除策略
+
+## lesson28 学习验证码
+    资料来自 // https://github.com/mojocn/base64Captcha
+    - 学习常见的我仲验证码 数字 字符串 数学验证码(加减乘除) 中文 语音
+    - 学习数字验证码
+    - 学会创建验证码和校验验证码 
+    - 思考前后端如何交互
+    
---
+++ lesson28/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/mojocn/base64Captcha"
+	"log"
+	"net/http"
+)
+
+// https://github.com/mojocn/base64Captcha
+//https://blog.csdn.net/zhanyia/article/details/105311794
+//DriverDigit config for captcha-engine-digit.  // 数据是存在内存中的，所有可以校验前端发来的验证码
+var store = base64Captcha.DefaultMemStore
+
+// base64Captcha.DefaultMemStore是默认的:10240 个 过期时间10分钟
+
+/*
+创建验证码驱动
+分为5种不同类型的图形验证码
+分别是什么样子如: https://captcha.mojotv.cn/
+dight 数字验证码  -> 演示这种
+audio 语音验证码
+string 字符验证码
+math 数学验证码(加减乘除)
+chinese中文验证码-有bug
+*/
+func Captcha(c *gin.Context) {
+	//字符,公式,验证码配置
+	// 生成默认数字的driver
+	driver := base64Captcha.NewDriverDigit(80, 240, 6, 0.7, 80)
+	cp := base64Captcha.NewCaptcha(driver, store)
+	if id, b64s, err := cp.Generate(); err != nil {
+		log.Println("获取验证码失败..", err)
+		c.JSON(http.StatusOK, gin.H{"message": "获取验证码成功"})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"message":   "获取验证码成功",
+			"CaptchaId": id,
+			"PicPath":   b64s,
+		})
+
+	}
+}
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/base/captcha", Captcha)
+
+	// 如发送 http://127.0.0.1:9090/base/verify?captchaId=Fh970kCUVnQHaVg6B0sE&captcha=634647
+	r.GET("/base/verify", func(c *gin.Context) {
+		CaptchaId := c.Query("captchaId")
+		Captcha := c.Query("captcha")
+
+		fmt.Printf("id:%s 字符:%s", CaptchaId, Captcha)
+		// http://tool.chinaz.com/tools/imgtobase/ 还原图片测试这个接口
+		// 同时在内存清理掉这个图片
+		if store.Verify(CaptchaId, Captcha, true) {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": "验证码成功",
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": "验证码错误",
+			})
+		}
+	})
+	r.Run(":9090")
+}
